d4b83d30b85473385d26171b6acf03a7d2035896
ptp <paartheepan94@gmail.com>
2020-10-04 17:36:29 +0200

    fix: add path to api in production

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000';
 
 const config = {
   headers: {
