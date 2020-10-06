61afd3aa5e94d9cdc84f33567ec829a395294633
ptp <paartheepan94@gmail.com>
2020-10-06 19:30:55 +0200

    style: add lint

--- server/src/services/channel.service.ts
+++ server/src/services/channel.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository, getRepository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Channel as ChannelEntity } from '../entities/channel.entity';
-import { GetChannelsQuery } from '../DTOs/request/query/getChannels.query.dto'
+import { GetChannelsQuery } from '../DTOs/request/query/getChannels.query.dto';
 import { Region as RegionEntity } from '../entities/region.entity';
 
 @Injectable()
@@ -16,60 +16,28 @@ export class ChannelService {
   async findAll(getChannelsQuery: GetChannelsQuery) {
     let channels = await this.channelsRepository
       .createQueryBuilder('channel')
-      .limit(50)
+      .limit(50);
     if (getChannelsQuery.range !== undefined) {
-      const range = getChannelsQuery.range.split('-')
-      channels = channels.andWhere('viewCount >= :min', { min: range[0] })
-      channels = channels.andWhere('viewCount <= :max', { max: range[1] })
+      const range = getChannelsQuery.range.split('-');
+      channels = channels.andWhere('viewCount >= :min', { min: range[0] });
+      channels = channels.andWhere('viewCount <= :max', { max: range[1] });
     }
-    let yolo1 = await channels.getMany()
-    let yolo = yolo1.map((a, i) => ({
+    const yolo1 = await channels.getMany();
+    const yolo = yolo1.map((a, i) => ({
       title: a.title,
-      subscriberCount: a.subscriberCount % 1000000 + 100000,
-      likePercentage: Math.floor(100 - 3 - (i / 3))
-    }))
+      subscriberCount: (a.subscriberCount % 1000000) + 100000,
+      likePercentage: Math.floor(100 - 3 - i / 3),
+    }));
     return [
       // JE MANQUE DE TEMPS OK ?!
-      [
-        yolo[0],
-        yolo[12],
-        yolo[15]
-      ],
-      [
-        yolo[44],
-        yolo[39],
-        yolo[19]
-      ],
-      [
-        yolo[39],
-        yolo[49],
-        yolo[6]
-      ],
-      [
-        yolo[0],
-        yolo[44],
-        yolo[6]
-      ],
-      [
-        yolo[25],
-        yolo[6],
-        yolo[15]
-      ],
-      [
-        yolo[37],
-        yolo[33],
-        yolo[24]
-      ],
-      [
-        yolo[33],
-        yolo[10],
-        yolo[49]
-      ],
-      [
-        yolo[18],
-        yolo[33],
-        yolo[35]
-      ]
-    ][Math.floor(Math.random() * 8)]
+      [yolo[0], yolo[12], yolo[15]],
+      [yolo[44], yolo[39], yolo[19]],
+      [yolo[39], yolo[49], yolo[6]],
+      [yolo[0], yolo[44], yolo[6]],
+      [yolo[25], yolo[6], yolo[15]],
+      [yolo[37], yolo[33], yolo[24]],
+      [yolo[33], yolo[10], yolo[49]],
+      [yolo[18], yolo[33], yolo[35]],
+    ][Math.floor(Math.random() * 8)];
   }
 }
