8366f3299f700674da534d7f82ebb15845f61731
ptp <paartheepan94@gmail.com>
2020-10-06 19:58:57 +0200

    chore: add client tests

--- client/tests/unit/example.spec.js
+++ client/tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import SectionTitle from '@/components/SectionTitle.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+    const wrapper = shallowMount(SectionTitle, {
+      propsData: { title: msg },
     });
     expect(wrapper.text()).to.include(msg);
   });
